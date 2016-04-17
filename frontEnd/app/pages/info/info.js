3f144226a0d39acc8652504a8b0b091a0856c8f2
labsterx <labsterx@gmail.com>
2016-04-18 01:57:45 +0800

    bug fixes

--- frontEnd/app/pages/info/info.js
+++ frontEnd/app/pages/info/info.js
@@ -1,4 +1,4 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
+import {Page, Platform, NavController, NavParams} from 'ionic-angular';
 import {BeerService} from '../services/BeerService';
 @Page({
   templateUrl: 'build/pages/info/info.html',
@@ -6,13 +6,20 @@ import {BeerService} from '../services/BeerService';
 })
 export class InfoPage {
   static get parameters() {
-    return [[NavController], [BeerService], [NavParams]];
+    return [[NavController], [BeerService], [NavParams], [Platform]];
   }
 
-  constructor(nav, beerService, navParams) {
+  constructor(nav, beerService, navParams, platform) {
     this.nav = nav;
     this.beerService = beerService;
     this.beer = navParams.get('beer');
+    this.platform = platform
+  }
+
+  launch(url) {
+      this.platform.ready().then(() => {
+          cordova.InAppBrowser.open(url, "_system", "location=true");
+      });
   }
 
 }
